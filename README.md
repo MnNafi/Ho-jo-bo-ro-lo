5595447d9573b5f806bd54ae9d835cd03b6b638d
MD. Mostofa Nowazish Nafi <mostofanowazish137@gmail.com>
2023-12-08 00:20:38 +0600

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# AtraiLLC_OfficialWebsite
---
+++ assets/js/header.js
@@ -0,0 +1,11 @@
+src = "https://code.jquery.com/jquery-3.6.4.min.js"
+
+$(function () {
+    // Load header.html into the element with id "header"
+    $('#header').load('../../components/header.html');
+});
+
+$(function () {
+    // Load footer.html into the element with id "footer"
+    $('#footer').load('../../components/footer.html');
+});
